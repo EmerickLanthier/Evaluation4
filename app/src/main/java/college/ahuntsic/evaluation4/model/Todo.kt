671eac18f7fae9ba6d85f1dd4be27668ca4d287c
madridboutin.g <madrid.bouting@gmail.com>
2025-04-30 14:02:14 -0400

    classe Todo

---
+++ app/src/main/java/college/ahuntsic/evaluation4/model/Todo.kt
@@ -0,0 +1,20 @@
+package college.ahuntsic.evaluation4.model
+
+import java.time.LocalDate
+
+enum class Priority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
+
+data class Todo (
+    val id : Int,
+    val dateCreation : LocalDate,
+    val name: String,
+    val note : String,
+    val completed : Boolean,
+    val endDate : LocalDate
+)
+
+
